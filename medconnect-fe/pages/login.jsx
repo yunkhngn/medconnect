e26bbcb0f2665ce0e6b31dd68e2d438c54c58a47
yunkhngn <rogkhoa.mail@gmail.com>
2025-09-27 03:08:24 +0700

    feat: create login and signup page

---
+++ medconnect-fe/pages/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Default } from '@/components/layouts'
+
+const login = () => {
+  return (
+    <Default>
+      <div>login</div>
+    </Default>
+  )
+}
+
+export default login
