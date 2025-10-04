b66e5fc38e7d22f6c25ffae02dd99ac0a79e0f61
yunkhngn <rogkhoa.mail@gmail.com>
2025-10-05 03:21:24 +0700

    feat: create search and apply page

--- medconnect-fe/components/layouts/Nav.jsx
+++ medconnect-fe/components/layouts/Nav.jsx
@@ -3,37 +3,6 @@ import navigate from "@/config/navigate";
 import { useRouter } from "next/router";
 import Image from "next/image";
 
-export const SearchIcon = ({size = 24, strokeWidth = 1.5, width, height, ...props}) => {
-  return (
-    <svg
-      aria-hidden="true"
-      fill="none"
-      focusable="false"
-      height={height || size}
-      role="presentation"
-      viewBox="0 0 24 24"
-      width={width || size}
-      {...props}
-    >
-      <path
-        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={strokeWidth}
-      />
-      <path
-        d="M22 22L20 20"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={strokeWidth}
-      />
-    </svg>
-  );
-};
-
-
 const Nav = () => {
   const router = useRouter();
 
@@ -43,14 +12,16 @@ const Nav = () => {
   return (
      <Navbar shouldHideOnScroll>
       <NavbarBrand>
-        <Image
-          src="assets/logo.svg"
-          alt="MedConnect Logo"
-          width={40}
-          height={40}
-          className="mr-2"
-        />
-        <p className="font-bold text-inherit">MedConnect</p>
+        <Link href="/">
+          <Image
+            src="assets/logo.svg"
+            alt="MedConnect Logo"
+            width={40}
+            height={40}
+            className="mr-2"
+          />
+          <p className="font-bold text-inherit">MedConnect</p>
+        </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         {navigate.route.map((item, index) => (
--- medconnect-fe/config/navigate.js
+++ medconnect-fe/config/navigate.js
@@ -1,8 +1,8 @@
 const navigate = {
   route: [
-    { text: "Chuyên khoa", link: "/" },
-    { text: "Tìm bác sĩ", link: "/about" },
-    { text: "Ứng tuyển bác sĩ", link: "/ung-tuyen-bac-si" },
+    { text: "Chuyên khoa", link: "/chuyen-khoa" },
+    { text: "Tìm bác sĩ", link: "/tim-kiem" },
+    { text: "Ứng tuyển bác sĩ", link: "/ung-tuyen" },
   ],
   button: [
     { text: "Đăng nhập", link: "/login", color: "primary", variant: "" },
