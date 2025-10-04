e2905d31a00fe1aeb68a21593f793a3cac0305df
HungNQ <hunghhll123@gmail.com>
2025-10-05 05:22:31 +0700

    feat: add home page

---
+++ medconnect-fe/pages/ung-tuyen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Default} from "@/components/layouts/"
+
+const ungTuyen = () => {
+  return (
+    <Default>
+        <div>Ứng tuyển</div>
+    </Default>
+  )
+}
+
+export default ungTuyen
