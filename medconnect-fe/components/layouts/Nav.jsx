c8f9685c21d63c6ad6b3a85fb70f688ababe3cac
yunkhngn <rogkhoa.mail@gmail.com>
2025-10-05 03:14:50 +0700

    chore: config nav bar

--- medconnect-fe/components/layouts/Nav.jsx
+++ medconnect-fe/components/layouts/Nav.jsx
@@ -1,19 +1,7 @@
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Input} from "@heroui/react";
 import navigate from "@/config/navigate";
 import { useRouter } from "next/router";
-
-export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
-};
+import Image from "next/image";
 
 export const SearchIcon = ({size = 24, strokeWidth = 1.5, width, height, ...props}) => {
   return (
@@ -55,7 +43,13 @@ const Nav = () => {
   return (
      <Navbar shouldHideOnScroll>
       <NavbarBrand>
-        <AcmeLogo />
+        <Image
+          src="assets/logo.svg"
+          alt="MedConnect Logo"
+          width={40}
+          height={40}
+          className="mr-2"
+        />
         <p className="font-bold text-inherit">MedConnect</p>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
--- medconnect-fe/config/navigate.js
+++ medconnect-fe/config/navigate.js
@@ -2,8 +2,7 @@ const navigate = {
   route: [
     { text: "Chuyên khoa", link: "/" },
     { text: "Tìm bác sĩ", link: "/about" },
-    { text: "Services", link: "/services" },
-    { text: "Blog", link: "/blog" },
+    { text: "Ứng tuyển bác sĩ", link: "/ung-tuyen-bac-si" },
   ],
   button: [
     { text: "Đăng nhập", link: "/login", color: "primary", variant: "" },
