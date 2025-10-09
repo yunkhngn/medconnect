6e6dc812f6d0a8f9f86afb36c4c8f0a5e1a3f156
yunkhngn <rogkhoa.mail@gmail.com>
2025-10-09 10:57:05 +0700

    chore: adding avatar

--- medconnect-fe/components/layouts/Nav.jsx
+++ medconnect-fe/components/layouts/Nav.jsx
@@ -1,4 +1,4 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Input} from "@heroui/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Avatar} from "@heroui/react";
 import navigate from "@/config/navigate";
 import { useRouter } from "next/router";
 import Image from "next/image";
@@ -75,20 +75,8 @@ const Nav = () => {
                 </Button>
           </NavbarItem>
         ))}
-        <NavbarItem className="hidden sm:flex w-48">
-          <Input
-                  classNames={{
-                    base: "max-w-full sm:max-w-[10rem] h-10",
-                    mainWrapper: "h-full",
-                    input: "text-small",
-                    inputWrapper:
-                      "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-                  }}
-                  placeholder="Type to search..."
-                  size="sm"
-                  startContent={<SearchIcon size={18} />}
-                  type="search"
-                />
+        <NavbarItem>
+           <Avatar src="/assets/homepage/mockup-avatar.jpg" />
         </NavbarItem>
       </NavbarContent>
     </Navbar>
