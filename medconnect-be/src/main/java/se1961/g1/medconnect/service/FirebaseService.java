98bfffc164c155403cb3a3adee611605a7334d48
yunkhngn <rogkhoa.mail@gmail.com>
2025-10-10 10:11:17 +0700

    chore: fix be

--- medconnect-be/src/main/java/se1961/g1/medconnect/service/FirebaseService.java
+++ medconnect-be/src/main/java/se1961/g1/medconnect/service/FirebaseService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se1961.g1.medconnect.enums.Services;
 
-import java.util.Arrays.*;
-
 @Service
 public class FirebaseService {
     @Autowired
