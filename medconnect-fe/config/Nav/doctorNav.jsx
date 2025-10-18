9b7887b314fd11047a81a8a18892b4e878419fca
HungNQ <hunghhll123@gmail.com>
2025-10-18 15:12:11 +0700

    remove tin-nhan page

--- medconnect-fe/config/Nav/doctorNav.jsx
+++ medconnect-fe/config/Nav/doctorNav.jsx
@@ -27,15 +27,6 @@ const menuItems = [
       href: '/bac-si/lich-lam-viec',
     },
     {
-      label: 'Tin nhắn',
-      icon: (
-        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-        </svg>
-      ),
-      href: '/bac-si/tin-nhan',
-    },
-    {
       label: 'Hồ sơ',
       icon: (
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
