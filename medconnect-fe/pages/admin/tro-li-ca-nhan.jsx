100fa2961ee931b1711016a4a1aa8387b968d9f8
yunkhngn <rogkhoa.mail@gmail.com>
2025-11-11 01:51:59 +0700

    fix: docker

--- medconnect-fe/pages/admin/tro-li-ca-nhan.jsx
+++ medconnect-fe/pages/admin/tro-li-ca-nhan.jsx
@@ -1,1 +1,8 @@
-
+export default function TroLiCaNhan() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Trợ Lí Cá Nhân (Admin)</h1>
+      <p>Trang đang được phát triển...</p>
+    </div>
+  );
+}
